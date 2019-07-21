61985f563a671abe81c771e1fc5a6051c19d098b
Aibek <aibek.abdykasymov@gmail.com>
2019-07-21 17:39:58 +0600

    add testing packages and settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
+import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
@@ -15,6 +16,9 @@ export default {
         file: 'public/bundle.js'
     },
     plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
         svelte({
             // enable run-time checks when not in production
             dev: !production,
