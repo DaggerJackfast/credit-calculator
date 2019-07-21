8223240c6af83c95c961dc45a6213424d3111489
Aibek <aibek.abdykasymov@gmail.com>
2019-07-21 17:40:32 +0600

    add caclucating module

---
+++ src/calculating.js
@@ -0,0 +1,17 @@
+class AnnuityCalculator{
+    constructor() {
+        
+    }
+    calculate(amount, percent, paymentMonthCount, startDate){
+        
+        reuturn [
+            {
+                monthlyPayment,
+                date,
+                percentAmount,
+                loanBalance
+            }
+        ]
+    }
+}
+
