3efe1d3924a6d09099f3568203f6485a1c5a3da9
Aibek <aibek.abdykasymov@gmail.com>
2019-07-28 14:17:57 +0600

    add differential payments calculating

--- src/calculating.js
+++ src/calculating.js
@@ -1,13 +1,56 @@
-export class AnnuityCalculator {
+export class Calculator {
     constructor() {}
-    calculate(amount, percent, monthCount, startDate) {
+    addMonths(stringDate, addedMonthsCount) {
+        const date = new Date(stringDate);
+        const addedYears = Math.floor(addedMonthsCount / 12);
+        const addedMonths = addedMonthsCount - (addedYears * 12);
+        if (addedYears) date.setFullYear(date.getFullYear() + addedYears);
+        if (addedMonths) date.setMonth(date.getMonth() + addedMonths);
+        const year = date.getFullYear();
+        const month = this.padWithZeroes(date.getMonth() + 1, 2);
+        const day = this.padWithZeroes(date.getDate(), 2);
+        const formatedDate = `${year}-${month}-${day}`;
+        return formatedDate;
+    }
+    padWithZeroes(number, length) {
+        let string = '' + number;
+        while (string.length < length) {
+            string = '0' + string;
+        }
+        return string;
+    }
+    numberRound(value, places) {
+        return +(Math.round(value + "e+" + places) + "e-" + places)
+    }
+    getMonthsDaysCount(stringDate) {
+        const date = new Date(stringDate);
+        const monthDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+        const daysCount = monthDate.getDate();
+        return daysCount;
+    }
+    getYearsDaysCount(stringDate) {
+        const date = new Date(stringDate);
+        const year = date.getFullYear();
+        const daysCount = this.isLeapYear(year) ? 366 : 365
+        return daysCount;
+    }
+    isLeapYear(year) {
+        return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+    }
+}
+
+export class AnnuityCalculator extends Calculator {
+    constructor() {
+        return super();
+    }
+    calculate(amount, percent, monthsCount, startDate) {
         const payments = [];
         const percentDecimal = percent / 100;
         const percentPerMonth = percentDecimal / 12;
-        const monthlyPayment = amount * (percentPerMonth + (percentPerMonth / (Math.pow((1 + percentPerMonth), monthCount) - 1)));
+        const monthlyPayment = amount * (percentPerMonth + (percentPerMonth / (Math.pow((1 + percentPerMonth), monthsCount) - 1)));
         let loanBalance = amount;
         let mainDebt = 0;
-        for (const index of Array(monthCount).keys()) {
+        for (const index of Array(monthsCount).keys()) {
             loanBalance = loanBalance - mainDebt;
             const percents = loanBalance * percentPerMonth;
             mainDebt = monthlyPayment - percents;
@@ -23,27 +66,33 @@ export class AnnuityCalculator {
         }
         return payments;
     }
-    addMonths(stringDate, addedMonthsCount) {
-        const date = new Date(stringDate);
-        const addedYears = Math.floor(addedMonthsCount / 12);
-        const addedMonths = addedMonthsCount - (addedYears * 12);
-        if (addedYears) date.setFullYear(date.getFullYear() + addedYears);
-        if (addedMonths) date.setMonth(date.getMonth() + addedMonths);
-        const year = date.getFullYear();
-        const month = this.padWithZeroes(date.getMonth(), 2);
-        const day = date.getDate();
-        const formatedDate = `${year}-${month}-${day}`;
-        return formatedDate;
-    }
+}
 
-    padWithZeroes(number, length) {
-        let string = '' + number;
-        while (string.length < length) {
-            string = '0' + string;
+export class DifferentialCalculator extends Calculator {
+    constructor() {
+        return super();
+    }
+    calculate(amount, percent, monthsCount, startDate) {
+        const payments = [];
+        const mainDebt = amount / monthsCount;
+        const percentDecimal = percent / 100;
+        for (const index of Array(monthsCount).keys()) {
+            const addedMonths = index + 1;
+            const date = this.addMonths(startDate, addedMonths);
+            const monthsDaysCount = this.getMonthsDaysCount(date);
+            const yearsDaysCount = this.getYearsDaysCount(date);
+            const loanBalance = amount - mainDebt * index;
+            const percents = loanBalance * percentDecimal * monthsDaysCount / yearsDaysCount;
+            const monthlyPayment = mainDebt + percents;
+            payments.push({
+                monthlyPayment: this.numberRound(monthlyPayment, 2),
+                loanBalance: this.numberRound(loanBalance, 2),
+                percents: this.numberRound(percents, 2),
+                mainDebt: this.numberRound(mainDebt, 2),
+                date,
+            })
         }
-        return string;
+        return payments;
     }
-    numberRound(value, places) {
-        return +(Math.round(value + "e+" + places) + "e-" + places)
-    }
+    
 }
--- tests/calculating.spec.js
+++ tests/calculating.spec.js
@@ -1,6 +1,14 @@
-import { assert } from 'chai';
-import {AnnuityCalculator} from '../src/calculating.js'
-import {readJsonFixture} from './test_utils.js';
+import {
+    assert
+} from 'chai';
+import {
+    AnnuityCalculator,
+    DifferentialCalculator,
+    Calculator
+} from '../src/calculating.js'
+import {
+    readJsonFixture
+} from './test_utils.js';
 describe('Array', function () {
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', function () {
@@ -9,15 +17,33 @@ describe('Array', function () {
     });
 });
 
-describe('AnnuityCalculator', function(){
-    it('should return pays for full period', function(){
+describe('AnnuityCalculator', function () {
+    it('should return pays for full period', function () {
         const amount = 100000;
         const percent = 10;
         const monthCount = 6;
-        const startDate = new Date('2015-01-24');
+        const startDate = new Date('2014-12-24');
         const calucaltor = new AnnuityCalculator();
         const payments = calucaltor.calculate(amount, percent, monthCount, startDate);
-        const expected = readJsonFixture('payments.json');
+        const expected = readJsonFixture('annuityPayments.json');
         assert.deepEqual(payments, expected);
+    })
+});
+describe('DifferentialCalculator', function () {
+    it('should return pays for full period', function () {
+        const amount = 300000;
+        const percent = 20;
+        const monthsCount = 6;
+        const startDate = new Date('2015-06-04');
+        const calucaltor = new DifferentialCalculator();
+        const payments = calucaltor.calculate(amount, percent, monthsCount, startDate);
+        const expected = readJsonFixture('differentialPayments.json');
+        assert.deepEqual(payments, expected);
+    })
+});
+describe('Calculator', function () {
+    it('should return date', function () {
+        const calculator = new Calculator();
+        assert.equal(calculator.addMonths('2015-06-04', 6), '2015-12-04');
     })
 });
