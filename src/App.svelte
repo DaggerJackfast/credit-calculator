59dcbb08ce388ce9f12fcf5ced11587d58bffff8
Aibek <aibek.abdykasymov@gmail.com>
2019-08-03 15:08:18 +0600

    Add selecting calculator type

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,37 @@
 <script>
   import { onMount } from "svelte";
   import { AnnuityCalculator, DifferentialCalculator } from "./calculating.js";
-  let amount = 100000;
-  let percent = 10;
-  let monthsCount = 6;
-  let startDate = "2015-01-04";
+  let amount = 0;
+  let percent = 0;
+  let monthsCount = 0;
+  let startDate = "";
+  let calculatorType = "annuity";
   let result = {
     payments: 0
-  }
+  };
 
   const annuity = new AnnuityCalculator();
-  function calcucatePayments(){
-    return annuity.calculate(amount, percent, monthsCount, startDate);
+  const differential = new DifferentialCalculator();
+  const calculators = {annuity, differential};
+  function calcucatePayments(type) {
+    return calculators[type].calculate(amount, percent, monthsCount, startDate);
   }
-  onMount(() => {
-    result = calcucatePayments();
-  });
   function handleSubmit() {
-    result = calcucatePayments();
+    result = calcucatePayments(calculatorType);
   }
 </script>
 
 <style>
+  .container {
+    max-width: 75%;
+    margin: 0 auto;
+  }
   .form-group {
     display: inline-block;
+  }
+  .form-group-full {
+    display: block;
+    width: 20%;
   }
   .input-group {
     display: flex;
@@ -52,6 +60,9 @@
   .form-button:active {
     background: #33d4e6;
   }
+  select.form-input {
+    background: transparent;
+  }
   input[type="date"].form-input {
     padding: 3px 5px;
   }
@@ -62,7 +73,6 @@
   }
   .result {
     display: block;
-    width: 75%;
   }
   .payment {
     display: flex;
@@ -71,11 +81,11 @@
     border-radius: 3px;
     margin-bottom: 3px;
   }
-  .payment-headers{
+  .payment-headers {
     font-weight: bold;
   }
-  .payment-total{
-    font-weight: 500;    
+  .payment-total {
+    font-weight: 500;
   }
   .payment-cell {
     width: 15%;
@@ -85,12 +95,23 @@
   }
 </style>
 
-<div>
+<div class="container">
   <h1>Credit calculator</h1>
   <div class="caclulator">
     <form
       class="form caclucalator-form"
       on:submit|preventDefault={handleSubmit}>
+      <div class="form-group form-group-full ">
+        <label for="calculator-type" class="form-label">Calculator type:</label>
+        <select
+          class="form-input"
+          id="calculator-type"
+          name="calculator-type"
+          bind:value={calculatorType}>
+          <option value="annuity">Annuity</option>
+          <option value="differential">Differential</option>
+        </select>
+      </div>
       <div class="form-group">
         <div class="input-group">
           <label for="amount" class="form-label">Amount:</label>
@@ -140,7 +161,7 @@
     </form>
     <div class="result">
       {#if result.payments.length > 0}
-      <h3>Payments:</h3>
+        <h3>Payments:</h3>
         <div class="payment payment-headers">
           <span class="payment-cell min-cell">#</span>
           <span class="payment-cell">payment</span>
